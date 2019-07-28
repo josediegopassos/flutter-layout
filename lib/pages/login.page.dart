37965866d28832aa4ae122ebe38c7669813e159a
Jose Diego Mariano de Oliveira Passos <josediegopassos@gmail.com>
2019-07-28 01:24:37 -0300

    Subindo implementações do carrinho

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -130,14 +130,7 @@ class LoginPage extends StatelessWidget {
                               color: Colors.white,
                             ),
                           ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomePage(),
-                              ),
-                            );
-                          },
+                          onPressed: () {},
                         ),
                       ),
                     ],
